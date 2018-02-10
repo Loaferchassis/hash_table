0b56b6dc3b5bd608e6e5e079f00cd3ac06cf6d85
loaferchassis <loaferchassis@gmail.com>
2018-02-10 16:25:20 +0300

    HashMap is now a class.

--- hash.py
+++ hash.py
@@ -1,41 +1,64 @@
-num_of_buckets = 100
-buckets = [[] for _ in range(num_of_buckets)]
+import time
 
 
 def main():
+    print('Hello!')
+
+    obj = HashMap()
     file = open('C:/Users/Alexander/PycharmProjects/tbms/text', encoding="utf8")
-    wordlist = file.readlines()
-    #print(wordlist)
-    for line in wordlist:
-        for word in line.split(" "):
-            insert(word)
-    #print(buckets[0])
-    #delete('give')
-    print(search('never'))
+    obj.build(file)
+
+    time1 = time.time()
+    # print(buckets[0])
+    # delete('give')
+    for i in range(1, 10000):
+        obj.search('never')
+
+    print(time.time() - time1)
 
 
-# hashFunction
-def hashFunction(key):
-    return hash(key) % num_of_buckets
+class HashMap:
+    def __init__(self):
+        self.num_of_buckets = 100
+        self.buckets = [[] for _ in range(self.num_of_buckets)]
 
+    def build(self, file):
+        wordlist = file.readlines()
+        # print(wordlist)
+        for line in wordlist:
+            for word in line.split(" "):
+                self.insert(word)
 
-# Insertion
-def insert(object):
-    buckets[hashFunction(object)].append(object)
+        words = []
+        for line in wordlist:
+            for word in line.split(" "):
+                words.append(word)
+        # time1 = time.time()
+        # for i in range(1, 10000):
+        #     for word in words:
+        #         if "never".__eq__(word):
+        #             break
+        # print(time.time() - time1)
 
+    # hashFunction
+    def hashFunction(self, key):
+        return hash(key) % self.num_of_buckets
 
-# Delete
-def delete(object):
-    buckets[hashFunction(object)].remove(object)
-    print('Delete')
+    # Insertion
+    def insert(self, object):
+        self.buckets[self.hashFunction(object)].append(object)
 
+    # Delete
+    def delete(self, object):
+        self.buckets[self.hashFunction(object)].remove(object)
+        print('Delete')
 
-# Search
-def search(object):
-    mHash = hashFunction(object)
-    for h in buckets[mHash]:
-        if object.__eq__(h):
-            return h, mHash
+    # Search
+    def search(self, object):
+        mHash = self.hashFunction(object)
+        for h in self.buckets[mHash]:
+            if object.__eq__(h):
+                return h, mHash
 
 
 if __name__ == '__main__':
